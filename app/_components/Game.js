4ad9f5464e36561f85576425e525679f7d99828e
jdelmundo23 <joshdelm@gmail.com>
2023-11-01 19:36:27 -0400

    moved input groups for game into Game component file

---
+++ app/_components/Game.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Guess from "./Guess.js"
+import { Inter } from "next/font/google"
+
+const inter = Inter({
+  subsets: ['latin'],
+})
+
+export default function Game() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [isGameOver, setGameOver] = useState(false);
+    function submitAnswer(e) {
+        e.preventDefault()
+        const answer = e.target[activeIndex]?.value;
+        if (testGuess(answer)) {
+          setGameOver(true);
+        } else {
+          setActiveIndex(activeIndex + 1);
+        }
+      }
+
+    return <form onSubmit={e => submitAnswer(e)} className="flex flex-col w-screen h-screen items-center space-y-8 justify-center justify-items-center bg-zinc-900">
+        <div className="flex flex-col space-y-3 h-[40%] w-[55%]">
+            {new Array(6).fill(null).map((_, i) =>
+                <Guess key={i} status={getBoxStatus(i, activeIndex, isGameOver)}></Guess>
+            )}
+        </div>
+        <div className="w-40 h-[5%]">
+            <button disabled={isGameOver || activeIndex > 5} className={`${inter.className} bg-white border rounded-sm w-full h-full text-3xl font-medium text-zinc-700 tracking-wider disabled:opacity-40 transition-colors duration-100`}>SUBMIT</button>
+        </div>
+    </form>
+}
+
+function getBoxStatus(index, activeIndex, isGameOver) {
+    return (
+        (activeIndex === index && !isGameOver) ? "active" :
+            activeIndex > index ? "wrong" :
+                activeIndex < index ? "pending" :
+                    "correct")
+}
+
+function testGuess(guess) {
+    return guess === "correct answer";
+}
--- app/page.js
+++ app/page.js
@@ -1,20 +1,12 @@
 'use client'
 import { useState, useEffect } from 'react';
-import { Inter } from "next/font/google"
-import Guess from "./_components/Guess.js"
-
-const inter = Inter({
-  subsets: ['latin'],
-})
+import Game from "./_components/Game.js"
 
 export default function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [isGameOver, setGameOver] = useState(false);
   const [query, setQuery] = useState('');
   const [token, setToken] = useState('');
   const [artists, setArtists] = useState([]);
 
-  console.log(artists[0]?.name);
   useEffect(() => {
     (async () => {
       const token = await getToken();
@@ -29,46 +21,17 @@ export default function App() {
       }
     })();
   }, [query]);
-  function submitAnswer(e) {
-    e.preventDefault()
-    const answer = e.target[activeIndex]?.value;
-    if (testGuess(answer)) {
-      setGameOver(true);
-    } else {
-      setActiveIndex(activeIndex + 1);
-    }
-  }
-  function getBoxStatus(index) {
-    return (
-      (activeIndex === index && !isGameOver) ? "active" :
-        activeIndex > index ? "wrong" :
-          activeIndex < index ? "pending" :
-            "correct")
-  }
   return (
-    <>
-      <input value={query} onChange={e => {setQuery(e.target.value)}}></input>
+    <div>
+      <input className={`w-[20%]`} value={query} onChange={e => {setQuery(e.target.value)}}></input>
       <ul>
-        {artists.map(artist => {
+        {query && artists.map(artist => {
           return <li key={artist.key}>{artist.name}</li>
         })}
       </ul>
-      <form onSubmit={e => submitAnswer(e)} className="flex flex-col w-screen h-screen items-center space-y-8 justify-center justify-items-center bg-zinc-900">
-        <div className="flex flex-col space-y-3 h-[40%] w-[55%]">
-          {new Array(6).fill(null).map((_, i) =>
-            <Guess key={i} status={getBoxStatus(i)}></Guess>
-          )}
-        </div>
-        <div className="w-40 h-[5%]">
-          <button disabled={isGameOver || activeIndex > 5} className={`${inter.className} bg-white border rounded-sm w-full h-full text-3xl font-medium text-zinc-700 tracking-wider disabled:opacity-40 transition-colors duration-100`}>SUBMIT</button>
-        </div>
-      </form>
-    </>
+      <Game/>
+    </div>
   )
-}
-
-function testGuess(guess) {
-  return guess === "correct answer";
 }
 
 async function getToken() {
